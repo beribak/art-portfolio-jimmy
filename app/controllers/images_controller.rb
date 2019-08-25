b11f70a9957f6c4e91c23bf81e47b621bf77f45e
Khoa Le <khoatle165@gmail.com>
2019-08-25 15:19:58 +0200

    add routes and methods for controllers

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,21 @@
 class ImagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
