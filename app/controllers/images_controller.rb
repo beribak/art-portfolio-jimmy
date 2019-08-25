2504c37b6e46c13f4e630037cd866a55510ce42e
Khoa Le <khoatle165@gmail.com>
2019-08-25 15:27:31 +0200

    finished with image controller logic, not testing yet

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,21 +1,50 @@
 class ImagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_image, only: [:edit, :update, :destroy]
 
   def index
+    @images = Image.all
   end
 
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_strong_params)
+    @image.user = current_user
+    if @image.save
+      redirect_to images_path
+    else
+      @image = Image.new
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @image.update(image_strong_params)
+    if @image.save
+      redirect_to image_path(@image)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @image.destroy
+    redirect_to images_path
+  end
+
+  private
+
+  def image_strong_params
+    params.require(:image).permit(:photo, :title, :description)
+  end
+
+  def set_image
+    @image = Image.find(params[:id])
   end
 end
