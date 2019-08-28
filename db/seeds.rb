96c329b2ef4554aa4a8df9ba6a32580736e156c6
danko beribak <beribakdanko@gmail.com>
2019-08-28 22:48:44 +0200

    inline block images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+khoa = User.create!(email: "khoa@gmail.com", password: "khoa123")
+danko = User.create!(email: "danko@gmail.com", password: "danko123")
+
+Image.create(photo: "https://picsum.photos/200/300", user: danko)
+Image.create(photo: 'https://unsplash.com/search/photos/random', user: danko)
+Image.create(photo: 'https://unsplash.com/search/photos/random', user: danko)
+Image.create(photo: 'https://unsplash.com/search/photos/random', user: danko)
+Image.create(photo: 'https://unsplash.com/search/photos/random', user: danko)
+Image.create(photo: 'https://unsplash.com/search/photos/random')
+Image.create(photo: 'https://unsplash.com/search/photos/random')
+Image.create(photo: 'https://unsplash.com/search/photos/random')
+Image.create(photo: 'https://unsplash.com/search/photos/random')
+Image.create(photo: 'https://unsplash.com/search/photos/random')
