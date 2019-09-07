7432461beb47b26a4f6d0324cf04a66da85c256c
Khoa Le <khoatle165@gmail.com>
2019-09-07 19:14:40 +0200

    add more about info

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@ infos = []
 infos << "Jimmy Cortés is a professional Hair and Make-Up Designer/Artist. After 20 years working in New York on Broadway and the Met Opera (and five more in regional opera), he fell in love with Barcelona, where he now lives."
 infos << "A first-generation Cuban-American, he was born and raised in Little Havana, Miami’s Cuban enclave, and grew up speaking Spanish and English. In fact, he attended the first bilingual school in the US - Coral Way Elementary. After graduating from Miami High School, Jimmy left his home town (and his Cuban bubble) to study Costume Design, before transferring to Wigs & Make-Up, at North Carolina School of the Arts. It was there - surrounded by so many non-Cubans for the first time - that he fully came to appreciate and celebrate his latin heritage."
 infos << "After leaving NCSA, he was hired first as an apprentice, then as staff, at Santa Fe Opera, and alternated between there and Virginia Opera for a few years, along with a handful of other regional opera companies. 25 years later, and he has worked in opera (regional, then ultimately 8 years at the Metropolitan Opera in New York) on Broadway (including Phantom, Lion King, and Wicked - greenifying 20 different witches over 6 years), TV (including Gossip Girl, Forever), charity events (including Broadway Bares, Gypsy of the Year), print (including headshots and album covers with Dirty Sugar, a Brooklyn-based photography team, and on various Broadway press features and events as a freelancer) and, for one year, a chance to work in New York Fashion Week. He has also designed for Broadway (Hands on a Hard Body) and off-Broadway (Queen of the Night)."
+infos << "While Jimmy has a broad range of theatrical and photographic/HD Make-Up skills, he specializes in characterization.\n
+  These skills include:
+  \n- Theatrical, Beauty & HD Make-Up
+  \n- Hair & Make-Up Design
+  \n- Period & Contemporary Hairstyling
+  \n- Wig-making - \"Ventilation\"
+  \n- Facial Hair Construction
+  \n- FX Make-Up/Prosthetics/Bald Pates
+  \n- Body Painting"
+infos << "Beyond Hair & Make-Up, Jimmy is sensitive to the individual needs of performers, and helps them get into character, putting them at ease before they go on stage or before the cameras. Having worked with artists from all around the world, he is flexible and open to dialogue, even when languages do not overlap. He sets high standards for himself in hygiene, communication and professionalism, which means he is trusted by many of the world's leading performing artists (including Sondra Radvanovsky, Jamie Barton, Miriam Margolyes, Elīna Garanča, Jonas Kaufmann, Rue McClanahan, Ben Vereen, Diana Damrau, Marcelo Álvarez, Kerry Ellis)."
+infos << "In 2017, Jimmy returned to Spain, the land of his grandparents, to focus on personal, collaborative projects, such as Atelier Café BCN, a collection of artist \"alter-ego portraits\", with husband Harry Hancock, an artist."
 
 jimmy= User.create(email: "testing@gmail.com", password: "testing", admin: true)
 infos.each do |info|
