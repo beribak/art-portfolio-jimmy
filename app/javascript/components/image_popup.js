ce2031a1ed71a60e01ac35e3ff170d25c10ff6fc
danko beribak <beribakdanko@gmail.com>
2019-09-09 18:27:53 +0200

    javascrypt pop up finished only need to add image description and transfer the code from assets/javascrypt to app/javacrypt

--- app/javascript/components/image_popup.js
+++ app/javascript/components/image_popup.js
@@ -1,5 +1,4 @@
-//= require rails-ujs
-//= require_tree .
+
 const imagePopUp = () => {
   // Get the modal
   var modal = document.getElementById("myModal");
@@ -14,14 +13,6 @@ const imagePopUp = () => {
     modalImg.src = this.src;
     }
   })
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
 }
 
-imagePopUp();
+export { imagePopUp };
