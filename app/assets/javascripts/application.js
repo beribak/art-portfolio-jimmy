00ebe00200fe8978552b49eab5131491af5b7747
danko beribak <beribakdanko@gmail.com>
2019-09-12 23:27:35 +0200

    commented out pop up from assets/javascript.js to try to deploy to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,27 @@
 //= require rails-ujs
 //= require_tree .
-const imagePopUp = () => {
-  // Get the modal
-  var modal = document.getElementById("myModal");
-  // alert("hgkgkjgkgkjhgh");
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
-  var img = document.querySelectorAll(".image");
-  var modalImg = document.getElementById("img01");
+// const imagePopUp = () => {
+//   // Get the modal
+//   var modal = document.getElementById("myModal");
+//   // alert("hgkgkjgkgkjhgh");
+//   // Get the image and insert it inside the modal - use its "alt" text as a caption
+//   var img = document.querySelectorAll(".image");
+//   var modalImg = document.getElementById("img01");
 
-  img.forEach(function(element){
-    element.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    }
-  })
+//   img.forEach(function(element){
+//     element.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     }
+//   })
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+//   // Get the <span> element that closes the modal
+//   var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-}
+//   // When the user clicks on <span> (x), close the modal
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+// }
 
-imagePopUp();
+// imagePopUp();
